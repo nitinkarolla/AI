091129b61ff5eff1a659accc803f74d415c9a62a
Aditya Vyas <adityavyas17@gmail.com>
2019-04-24 21:35:07 -0400

    make small edits

--- Colorization/NeuralNetwork.py
+++ Colorization/NeuralNetwork.py
@@ -13,16 +13,20 @@ class NeuralNetwork():
         self.neuronsEachLayer = neuronsEachLayer
 
     def weightsInitialisation(self):
+
         #Initialising a numpy array of dim(hiddenlayers, neurons) to store weights
         self.weights = np.empty((self.hiddenLayers, self.neuronsEachLayer), dtype = object)
         for i in range(self.hiddenLayers):
             for j in range(self.neuronsEachLayer):
+
                 #first hidden layer
                 if i == 0:
-                    self.weights[i,j] = np.random.normal(0,0.5, size = 1 + self.X.shape[1])
+                    self.weights[i,j] = np.random.normal(0, 0.5, size = 1 + self.X.shape[1])
+
                 #rest hidden layers
                 else:
-                    self.weights[i,j] = np.random.normal(0,0.5, size = 1 + self.neuronsEachLayer)
+                    self.weights[i,j] = np.random.normal(0, 0.5, size = 1 + self.neuronsEachLayer)
+
         #Weights for the final output layer
         self.outputLayerWeights =  np.random.normal(0,0.5, size = 1 + self.neuronsEachLayer)
     
@@ -36,18 +40,21 @@ class NeuralNetwork():
         x.append(1.0)  
         for i in range(self.hiddenLayers + 1):
             outputFromCurrLayer = []
+
             #For first Layer
             if i == 0:
                 for j in range(self.neuronsEachLayer):
                     outputFromCurrLayer.append(self.activationHidden(np.dot(self.weights[i,j],x)))
                 outputFromCurrLayer.append(1.0)
                 outputFromPrevLayer = outputFromCurrLayer.copy()
+
             #output Layer
             elif i == self.hiddenLayers:
-                return self.activationOut(np.dot(self.outputLayerWeights,outputFromPrevLayer))
+                return self.activationOut(np.dot(self.outputLayerWeights, outputFromPrevLayer))
+
             #Rest all Layers
             else:
                 for j in range(self.neuronsEachLayer):
-                    outputFromCurrLayer.append(self.activationHidden(np.dot(self.weights[i,j],outputFromPrevLayer)))
+                    outputFromCurrLayer.append(self.activationHidden(np.dot(self.weights[i,j], outputFromPrevLayer)))
                 outputFromCurrLayer.append(1.0)
                 outputFromPrevLayer = outputFromCurrLayer.copy()
