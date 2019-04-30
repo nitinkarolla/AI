c66146659dc4beedcff09af120bd59b0af0693bc
Aditya Vyas <adityavyas17@gmail.com>
2019-04-29 22:36:04 -0400

    neural network completed

--- Colorization/NeuralNetwork.py
+++ Colorization/NeuralNetwork.py
@@ -5,144 +5,194 @@ from copy import deepcopy
 
 class NeuralNetwork():
 
-    def __init__(self, num_hidden_layers = 2, learning_rate = 0.01, num_neurons_each_layer = None, weights = None):
+    SigmoidActivation = "sigmoid"
+    ReLUActivation = "relu"
+    LinearActivation = "linear"
+
+    def __init__(self,
+                 num_hidden_layers = 2,
+                 learning_rate = 0.01,
+                 num_neurons_each_layer = None,
+                 epochs = 10,
+                 weights = None):
         self.weights = weights
         self.num_hidden_layers = num_hidden_layers
         self.num_neurons_each_layer = num_neurons_each_layer
-        self.activationHidden = self.sigmoid
-        self.activationOut = self.sigmoid
         self.learning_rate = learning_rate
+        self.epochs = epochs
 
-    def sigmoid(self, x):
-        return 1 / (1 + math.exp(-x))
+        # Sigmoid activation for other layers. Linear activation for last layer
+        self.activations = [self.SigmoidActivation] * self.num_hidden_layers + [self.LinearActivation]
+        self.activations_functions = {
+            self.SigmoidActivation: self._sigmoid,
+            self.ReLUActivation: self._relu,
+            self.LinearActivation: self._linear
+        }
+        self.activations_derivatives = {
+            self.SigmoidActivation: self._sigmoid_derivative,
+            self.ReLUActivation: self._relu_derivative,
+            self.LinearActivation: self._linear_derivative
+        }
 
-    def sigmoid_derivative(self, x):
-        return self.sigmoid(x) * (1 - self.sigmoid(x))
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
 
-    def _square_loss(self, pred, y):
-        return (pred - y) ** 2
+    def _relu(self, x):
+        return
+
+    def _linear(self, x):
+        return x
+
+    def _sigmoid_derivative(self, x):
+        return self._sigmoid(x) * (1 - self._sigmoid(x))
+
+    def _relu_derivative(self, x):
+        return
+
+    def _linear_derivative(self, x):
+        return 1
+
+    def _mse_loss(self, pred, y):
+        return np.mean((pred - y) ** 2)
 
     def _initialise_weights(self, input_shape):
 
         self.num_neurons_each_layer.append(1)
         self.total_layers = self.num_hidden_layers + 1
+        self.layers = range(self.total_layers)
 
         # Initialising a numpy array of
         # shape = (number of hidden layers, number of neurons, number of weights per neuron) to store weights
-        self.weights = np.empty(shape = (self.total_layers, self.num_neurons_each_layer, None))
-        print(self.weights)
+        self.weights = []
 
         # Iterate through the layers
-        for layer in range(self.total_layers):
+        for layer in self.layers:
             self.weights.append([])
 
-            # Iterate through neurons in this layer
             number_of_neurons_in_this_layer = self.num_neurons_each_layer[layer]
-            for _ in range(number_of_neurons_in_this_layer):
+            if layer == 0:
+                self.weights[layer] = np.random.normal(0, 0.5, size = (number_of_neurons_in_this_layer, input_shape))
+            else:
+                self.weights[layer] = np.random.normal(0, 0.5, size = (number_of_neurons_in_this_layer,
+                                                     1 + self.num_neurons_each_layer[layer - 1]))
 
-                # first hidden layer
-                if layer == 0:
-                    self.weights[layer].append(np.random.normal(0, 0.5, size = input_shape))
+        self.weights = np.array(self.weights)
 
-                # rest hidden layers
-                else:
-                    # Each neuron will have number of weights equal to the number of neuron outputs from the
-                    # previous layer. The first weight for each neuron is the bias weight.
-                    self.weights[layer].append(np.random.normal(0, 0.5, size = 1 + self.num_neurons_each_layer[layer - 1]))
-
-    def _update_weights(self, out, output_derivatives):
-        for curr_layer in range(self.total_layers):
-            next_layer = curr_layer + 1
-            next_layer_outputs = out[next_layer][1:]
-            curr_layer_outputs = out[curr_layer]
-            for curr_layer_neuron in range(self.num_neurons_each_layer[curr_layer]):
-                sigmoid_der = self.sigmoid_derivative(next_layer_outputs[curr_layer_neuron])
-                self.weights[curr_layer][curr_layer_neuron] -= self.learning_rate * \
-                    output_derivatives[next_layer][curr_layer_neuron] * sigmoid_der * curr_layer_outputs[curr_layer_neuron]
-
-    def _backward(self, y, out):
+    def _backward(self, x, y, out):
 
         # The derivatives array will have the same shape as weights array. - one derivative for each
         # weight
-        self.weight_derivatives = deepcopy(self.weights)
         output_derivatives = deepcopy(out)
+        weight_derivatives = deepcopy(self.weights)
 
-        # Calculate the output derivatives w.r.t loss
-        layers_reversed = range(self.total_layers - 1, -1, -1)
+        # Compute the output derivatives
+        layers_reversed = self.layers[::-1]
         for curr_layer in layers_reversed:
 
-            # We start from 1st index to neglect the bias term at the start of the output
-            outputs_of_current_layer = out[curr_layer][1:]
-            curr_layer_neurons = self.num_neurons_each_layer[curr_layer]
+            # Get the activation for this layer and its derivative function
+            activation_for_this_layer = self.activations[curr_layer]
+            activation_derivative = self.activations_derivatives[activation_for_this_layer]
 
-            # Cycle through the neurons in this layer and calculate the
-            # derivative of output from each neuron.
-            for curr_layer_neuron in range(curr_layer_neurons):
+            # For the last layer simply use the formula
+            if curr_layer == self.total_layers - 1:
+                output_derivatives[curr_layer] = 2*(out[curr_layer] - y)
+                continue
 
-                # For last layer use the gradient formula directly
-                if curr_layer == self.total_layers - 1:
-                    output_derivatives[curr_layer][curr_layer_neuron + 1] = 2 * (
-                            outputs_of_current_layer[curr_layer_neuron] - y)
+            # For the other layers use the derivatives from previous layers
+            next_layer = curr_layer + 1
+            first_term = output_derivatives[next_layer]
 
-                # For the rest of the layers
-                else:
+            # Calculate the sigmoid derivative
+            current_layer_output = out[curr_layer].copy()
+            current_layer_output = np.insert(current_layer_output, obj = 0, values = 1)
+            sigma_derivative = activation_derivative(self.weights[next_layer] @ current_layer_output)
 
-                    # Sum over all the neurons in the next layer.
-                    next_layer = curr_layer + 1
-                    next_layer_neurons = self.num_neurons_each_layer[next_layer]
-                    next_layer_outputs = out[next_layer][1:]
+            for curr_layer_neuron in range(self.num_neurons_each_layer[curr_layer]):
 
-                    derivative = 0
-                    for next_layer_neuron in range(next_layer_neurons):
-                        sigmoid_der = self.sigmoid_derivative(next_layer_outputs[next_layer_neuron])
-                        second_term = self.weights[next_layer][next_layer_neuron][curr_layer_neuron]
-                        derivative += output_derivatives[next_layer][next_layer_neuron] * sigmoid_der * second_term
+                # Calculate the second term of the output derivative. We multiply only those weights which
+                # are at the index of current layer neuron. Also, we remove the bias from the weights.
+                next_layer_weights_without_bias = self.weights[next_layer][:, 1:]
+                second_term = sigma_derivative * next_layer_weights_without_bias[:, curr_layer_neuron]
+                output_derivatives[curr_layer][curr_layer_neuron] = first_term @ second_term
 
-                    output_derivatives[curr_layer][curr_layer_neuron + 1] = derivative
+        # Compute the weight derivatives
+        for curr_layer in layers_reversed:
 
-        # Make bias term derivatives 0
-        for index in range(len(output_derivatives)):
-            output_derivatives[index][0] = 0
+            # Get the activation for this layer and its derivative function
+            activation_for_this_layer = self.activations[curr_layer]
+            activation_derivative = self.activations_derivatives[activation_for_this_layer]
 
-        # Update the weights based on the above computed derivatives
-        self._update_weights(out, output_derivatives)
+            # If first layer then use the data as the previous layer.
+            if curr_layer == 0:
+                previous_layer_output = x
+            else:
+                prev_layer = curr_layer - 1
+                previous_layer_output = out[prev_layer].copy()
+                previous_layer_output = np.insert(previous_layer_output, obj = 0, values = 1)
+
+            for curr_layer_neuron in range(self.num_neurons_each_layer[curr_layer]):
+                first_term = output_derivatives[curr_layer][curr_layer_neuron]
+                sigma_derivative = activation_derivative(self.weights[curr_layer][curr_layer_neuron] @ previous_layer_output)
+                second_term = sigma_derivative * previous_layer_output
+                weight_derivatives[curr_layer][curr_layer_neuron] = first_term * second_term
+
+        # Update the weights
+        self.weights -= weight_derivatives
 
     def _forward(self, x):
         out = []
-        for curr_layer in range(self.total_layers):
-
-            # We append a 1 to the output of each layer for the bias weight
-            out.append([1])
+        for curr_layer in self.layers:
+            out.append([])
 
             # Multiply the input with each neuron's weights in this layer
             # and store the output of each neuron
-            for weights in self.weights[curr_layer]:
-                if curr_layer == 0:
-                    out[curr_layer].append(self.sigmoid(weights.T @ x))
-                else:
-                    previous_layer_output = out[curr_layer - 1]
-                    out[curr_layer].append(self.sigmoid(weights.T @ previous_layer_output))
-
+            weights = self.weights[curr_layer]
+            activation_for_this_layer = self.activations[curr_layer]
+            activation_function = self.activations_functions[activation_for_this_layer]
+            if curr_layer == 0:
+                out[curr_layer] = activation_function(weights @ x)
+            else:
+                previous_layer_output = out[curr_layer - 1].copy()
+                previous_layer_output = np.insert(previous_layer_output, obj = 0, values = 1)
+                out[curr_layer] = activation_function(weights @ previous_layer_output)
         return out
 
     def fit(self, X, y):
 
         # Add a bias column to X
-        X = np.column_stack((np.ones(len(X)), X))
-        data = zip(*(X, y))
+        X_new = np.column_stack((np.ones(len(X)), X))
+        data = zip(*(X_new, y))
 
         # Initialise the weights of the network
-        self._initialise_weights(X.shape[1])
+        self._initialise_weights(X_new.shape[1])
 
-        # Update weights using gradient descent. For this we do a forward pass and backward pass
-        # for each data point and update weights after each pass.
-        for x, y in data:
-            out = self._forward(x)
-            pred = out[-1][-1]
-            loss = self._square_loss(pred, y)
-            self._backward(y, out)
+        for epoch in range(self.epochs):
 
+            # Update weights using gradient descent. For this we do a forward pass and backward pass
+            # for each data point and update weights after each pass.
+            for x_, y_ in data:
+                out = self._forward(x_)
+                self._backward(x_, y_, out)
+
+            predictions = self.predict(X)
+            loss = self._mse_loss(predictions, y)
+            print("Epoch = ", str(epoch + 1), " - ", "Loss = ", str(loss))
+
+    def predict(self, X):
+
+        # Add a bias column to X
+        X_new = np.column_stack((np.ones(len(X)), X))
+
+        preds = []
+        for x in X_new:
+            pred = self._forward(x)[-1][-1]
+            preds.append(pred)
+
+        preds = np.array(preds)
+        return preds
 
 x = np.random.randint(-50, 50, (5, 3))
-nn = NeuralNetwork(num_hidden_layers = 2, num_neurons_each_layer = [2, 3])
-nn.fit(X = x, y = [1, 0, 1, 0])
+nn = NeuralNetwork(num_hidden_layers = 4, num_neurons_each_layer = [2, 3, 3, 4], epochs = 10000)
+nn.fit(X = x, y = [1, 0, 1, 0, 0])
+print(nn.weights)
+print(nn.predict(x))
