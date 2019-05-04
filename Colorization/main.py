a5930dd7eaaec9817db749adffad7693416aa4be
vedantc6 <vedantc6@gmail.com>
2019-05-04 14:19:35 -0400

    pickled models

--- Colorization/main.py
+++ Colorization/main.py
@@ -8,9 +8,10 @@ import os
 import pandas as pd
 from NeuralNetwork2 import NeuralNetwork
 from sklearn.preprocessing import StandardScaler
+import pickle
 
 class Colorizer():
-    def __init__(self, red_list, blue_list, green_list, image_location):
+    def __init__(self, red_list, blue_list, green_list, image_location, epochs, f_s, result_size=300):
         self.im_size = (0, 0)
         self.image_loc = image_location
         self.image = None
@@ -19,7 +20,10 @@ class Colorizer():
         self.red = red_list
         self.blue = blue_list
         self.green = green_list
-        
+        self.result_img_size = result_size
+        self.epochs = epochs
+        self.f_s = f_s
+
     def extract_pixels(self):
         # Open the image
         self.image = Image.open(self.image_loc)
@@ -31,7 +35,7 @@ class Colorizer():
         self.pixel_values = list(self.image.getdata())
         
     def create_image_from_array(self):
-        w, h = 100, 100
+        w, h = self.result_img_size, self.result_img_size
         count = 0
         data = np.zeros((h, w, 3), dtype=np.uint8)
         
@@ -41,13 +45,14 @@ class Colorizer():
                 count +=1
         
         img = Image.fromarray(data, 'RGB')
-        img.save('homemade_scene5_p300_e30_l_3.png')
+        img.save("./Results/" + "epochs_" + str(self.epochs) + "f_s_" + str(self.f_s) + "_" + self.image_loc.split("/")[-1])
         img.show()
 
 class ImageData():
-    def __init__(self, f_s, directory):
+    def __init__(self, f_s, directory, image_size=300):
         self.f_s = f_s
         self.directory = directory
+        self.img_size = image_size
 
     def pad_images(self, image):
         p = int((self.f_s - 1)/2)
@@ -86,7 +91,7 @@ class ImageData():
                     if f.split(".")[1] in exts:
                         print("Accessing ", f)
                         image = cv2.imread(os.path.join(root, f))
-                        image = cv2.resize(image, (100,100), interpolation = cv2.INTER_AREA)
+                        image = cv2.resize(image, (self.img_size,self.img_size), interpolation = cv2.INTER_AREA)
                         # gray image
                         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
                         # red, green, blue components
@@ -142,7 +147,8 @@ class ImageData():
 if __name__ == '__main__':
     # Getting images and returning X, y and file_name lists
     # Use indexes in X, y and file_name to get values for respective images
-    image_data = ImageData(f_s=7, directory="./Images/")
+    f_s = 7
+    image_data = ImageData(f_s=f_s, directory="./Images/", image_size=300)
     X, y, file_name = image_data.get_images()
     data_X, data_y_red, data_y_green, data_y_blue = image_data.align_data(X, y)
     
@@ -151,43 +157,63 @@ if __name__ == '__main__':
 
     X_train = scaler.transform(data_X)
 
-    # X = np.random.rand(4, 3)
-    # y = np.array([1, 0, 0, 1])
-    nn_r= NeuralNetwork(epochs = 30,
-                        batch_size = 100,
-                        num_hidden_layers = 3,
-                        num_neurons_each_layer = [10, 20, 10],
-                        learning_rate = 0.003)
-    nn_g= NeuralNetwork(epochs = 30,
-                        batch_size = 100,
-                        num_hidden_layers = 3,
-                        num_neurons_each_layer = [10, 20, 10],
-                        learning_rate = 0.003)
-    nn_b= NeuralNetwork(epochs = 30,
-                        batch_size = 100,
-                        num_hidden_layers = 3,
-                        num_neurons_each_layer = [10, 20, 10],
-                        learning_rate = 0.003)
+    # # TRAINING
+    # nn_r= NeuralNetwork(epochs = 10,
+    #                     batch_size = 100,
+    #                     num_hidden_layers = 3,
+    #                     num_neurons_each_layer = [10, 20, 10],
+    #                     learning_rate = 0.003)
+    # nn_g= NeuralNetwork(epochs = 10,
+    #                     batch_size = 100,
+    #                     num_hidden_layers = 3,
+    #                     num_neurons_each_layer = [10, 20, 10],
+    #                     learning_rate = 0.003)
+    # nn_b= NeuralNetwork(epochs = 10,
+    #                     batch_size = 100,
+    #                     num_hidden_layers = 3,
+    #                     num_neurons_each_layer = [10, 20, 10],
+    #                     learning_rate = 0.003)
     
-    print("Training for red model\n-------------------------")
-    nn_r.fit(X_train, data_y_red)
-    print("-------------------------\nTraining for green model\n-------------------------")
-    nn_g.fit(X_train, data_y_green)
-    print("-------------------------\nTraining for blue model\n-------------------------")
-    nn_b.fit(X_train, data_y_blue)
+    # print("Training for red model\n-------------------------")
+    # nn_r.fit(X_train, data_y_red)
+    # print("-------------------------\nTraining for green model\n-------------------------")
+    # nn_g.fit(X_train, data_y_green)
+    # print("-------------------------\nTraining for blue model\n-------------------------")
+    # nn_b.fit(X_train, data_y_blue)
     
+    filename_red = 'finalized_model_red.sav'
+    # pickle.dump(nn_r, open(filename_red, 'wb'))
+    filename_green = 'finalized_model_green.sav'
+    # pickle.dump(nn_g, open(filename_green, 'wb'))
+    filename_blue = 'finalized_model_blue.sav'
+    # pickle.dump(nn_b, open(filename_blue, 'wb'))
+
+    # TESTING
     directory = "./Images/test"
-    X, y, files = image_data.get_images(directory, "test")
+    test_image = './Images/test/scene5.jpeg'
+
+    X_test, y_test, files = image_data.get_images(directory, "test")
     print(files)
-    data_X_test, data_y_red_test, data_y_green_test, data_y_blue_test = image_data.align_data(X, y)
+    data_X_test, data_y_red_test, data_y_green_test, data_y_blue_test = image_data.align_data(X_test, y_test)
     data_X_test = scaler.transform(data_X_test)
     
-    test_predictions_b = nn_b.predict(data_X_test)
-    test_predictions_g = nn_g.predict(data_X_test)
-    test_predictions_r = nn_r.predict(data_X_test)
+    loaded_model_b = pickle.load(open(filename_blue, 'rb'))
+    loaded_model_g = pickle.load(open(filename_green, 'rb'))
+    loaded_model_r = pickle.load(open(filename_red, 'rb'))
+
+    test_predictions_b = loaded_model_b.predict(data_X_test)
+    test_predictions_g = loaded_model_g.predict(data_X_test)
+    test_predictions_r = loaded_model_r.predict(data_X_test)
     
-    new_color = Colorizer(red_list=test_predictions_r, blue_list=test_predictions_b, green_list=test_predictions_g, image_location='./Images/test/scene5.jpeg')
+    new_color = Colorizer(red_list=test_predictions_r, 
+                            blue_list=test_predictions_b, 
+                            green_list=test_predictions_g, 
+                            image_location=test_image, 
+                            result_size=300,
+                            epochs=loaded_model_b.epochs,
+                            f_s=f_s
+                            )
     new_color.extract_pixels()
     new_color.create_image_from_array()
-    print("Done")
+    print("Done Colorization")
     
